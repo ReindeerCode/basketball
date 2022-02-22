a8ebda0071054b08485cca80f56a608edfefd383
MJRudolph <ReindeerCode@gmail.com>
2022-02-21 20:02:09 -0500

    changed default React App.js code to Bulma code to confirm working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "bulma/css/bulma.min.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div classname="main">
+      <div className="buttons">
+        <button class="button is-primary">Primary</button>
+        <button class="button is-link">Link</button>
+        <button class="button is-info">Info</button>
+        <button class="button is-success">Success</button>
+        <button class="button is-warning">Warning</button>
+        <button class="button is-danger">Danger</button>
+        <button class="button is-black">Black</button>
+        <button class="button is-white">White</button>
+        <button class="button is-dark">Dark</button>
+        <button class="button is-light">Light</button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
